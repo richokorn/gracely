94cdeec29b0f0c912dc5b8db9b8f55611704883a
Richard Korn <richard.korn@bearingpoint.com>
2025-07-22 16:39:45 +0200

    [master] Amend topbar.component; Add styling; Add layout.service

--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -1,14 +1,31 @@
-import { Component, input, InputSignal } from '@angular/core';
+import { Component, computed, inject, input, InputSignal } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { LangConfig } from '../../../types/lang-config.type';
+import { Button } from 'primeng/button';
+import { LayoutService } from '../../services/layout.service';
+import { Select } from 'primeng/select';
+import { TranslatePipe } from '@ngx-translate/core';
+import { FormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
+import { StyleClass } from 'primeng/styleclass';
 
 @Component({
   selector: 'app-topbar',
-  imports: [],
+  imports: [Button, Select, TranslatePipe, FormsModule, NgClass, StyleClass],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.scss',
+  host: { class: 'topbar' },
 })
 export class TopbarComponent {
+  layoutService: LayoutService = inject(LayoutService);
   langConfig: InputSignal<LangConfig> = input.required();
+  isDarkMode = computed(() => this.layoutService.appState().darkMode);
   menuItems: InputSignal<MenuItem[]> = input.required();
+
+  toggleDarkMode() {
+    this.layoutService.appState.update((state) => ({
+      ...state,
+      darkMode: !state.darkMode,
+    }));
+  }
 }
