3e2d92368e6b5c5f4ed3e863cb9d89b706b27f51
Richard Korn <richard.korn@bearingpoint.com>
2025-07-26 03:53:48 +0200

    [master] Bind service into writer-widget.component

--- src/app/components/writer-widget/writer-widget.component.ts
+++ src/app/components/writer-widget/writer-widget.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  inject,
+  ViewChild,
+} from '@angular/core';
+import { InputText } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { WriterService } from '../../services/writer.service';
 
 @Component({
   selector: 'app-writer-widget',
-  imports: [],
+  imports: [InputText, FormsModule, InputText],
   templateUrl: './writer-widget.component.html',
   styleUrl: './writer-widget.component.scss',
   host: {
-    class: 'app-writer-widget layout-card col-item-1',
+    class: 'layout-card col-item-1',
   },
 })
-export class WriterWidgetComponent {}
+export class WriterWidgetComponent implements AfterViewInit {
+  @ViewChild('textAreaElement')
+  textAreaElement!: ElementRef<HTMLTextAreaElement>;
+  writerService: WriterService = inject(WriterService);
+
+  ngAfterViewInit() {
+    this.writerService.registerTextArea(this.textAreaElement);
+    this.writerService.updateTextarea();
+  }
+
+  onInput(): void {
+    this.writerService.onInput();
+  }
+}
